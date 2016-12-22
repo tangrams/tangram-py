08b66163915ff55fc84352e9f4a1e385095bafcf
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-12-22 10:29:07 -0500

    gps3

--- examples/gps.py
+++ examples/gps.py
@@ -6,17 +6,17 @@
 import sys, gps3
 the_connection = gps3.GPSDSocket() 
 the_fix = gps3.Fix()
-try:
-    for new_data in the_connection:
-        if new_data:
-            the_fix.refresh(new_data)
-        if not isinstance(the_fix.TPV['lat'], str): # lat as determinate of when data is 'valid'
-            speed = the_fix.TPV['speed']
-            latitude = the_fix.TPV['lat']
-            longitude = the_fix.TPV['lon']
-            altitude  = the_fix.TPV['alt']
 
-            print (latitude, longitude)
+for new_data in the_connection:
+    if new_data:
+        the_fix.refresh(new_data)
+    if not isinstance(the_fix.TPV['lat'], str): # lat as determinate of when data is 'valid'
+        speed = the_fix.TPV['speed']
+        latitude = the_fix.TPV['lat']
+        longitude = the_fix.TPV['lon']
+        altitude  = the_fix.TPV['alt']
+
+        print (latitude, longitude)
 
 # sys.path.append('../')
 # from tangram import TangramMap
